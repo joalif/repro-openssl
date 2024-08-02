b6153dfa792568a8a7e04ba0a9913fe0b8bee256
Ubuntu <ubuntu@noble-ssl.lxd>
2024-08-02 11:56:25 +0000

    Add loop in main

--- main.py
+++ main.py
@@ -12,8 +12,30 @@ def run_cf_api():
       pass
   return 'Finished'
 
-if __name__ == "__main__":
+
+def measure():
   start = time.time()
   run_cf_api()
   stop = time.time()
-  print(stop-start)
+  mili = int((stop-start)*1000)
+  return mili
+
+
+
+if __name__ == "__main__":
+  l = []
+  for i in range(0,10):
+    mes = measure()
+    print(mes)
+    l.append(mes)
+  over_8 = list(filter(lambda x: (x>= 800 and x< 900), l))
+  over_9 = list(filter(lambda x: (x>= 900 and x< 1000), l))
+  over_10 = list(filter(lambda x: (x>= 1000), l))
+  print("Over 800ms:")
+  print(len(over_8))
+  print("Over 900ms:")
+  print(len(over_9))
+  print("Over 1000ms:")
+  print(len(over_10))
+  for i in over_10:
+      print(i)
