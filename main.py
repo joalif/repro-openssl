868892727cf6397f867aea85f9a9b33cc0f2a651
Ioanna Alifieraki <ioanna-maria.alifieraki@canonical.com>
2024-08-02 16:50:02 +0300

    Add median

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import concurrent.futures
 import time
+from statistics import median
 
 def runHTTPFunction():
   return requests.get('https://www.google.com/humans.txt').content.decode("UTF-8")
@@ -21,21 +22,26 @@ def measure():
   return mili
 
 
+def roundup(x):
+    return x if x % 100 == 0 else x + 100 - x % 100
+
 
 if __name__ == "__main__":
-  l = []
-  for i in range(0,10):
-    mes = measure()
-    print(mes)
-    l.append(mes)
-  over_8 = list(filter(lambda x: (x>= 800 and x< 900), l))
-  over_9 = list(filter(lambda x: (x>= 900 and x< 1000), l))
-  over_10 = list(filter(lambda x: (x>= 1000), l))
-  print("Over 800ms:")
-  print(len(over_8))
-  print("Over 900ms:")
-  print(len(over_9))
-  print("Over 1000ms:")
-  print(len(over_10))
-  for i in over_10:
-      print(i)
+  n = [10,20,50,100,500,1000]
+  for j in n:
+    print("######Results for %d j runs######" % len(range(0,j)))
+    l = []
+    for i in range(0,j):
+      mes = measure()
+      l.append(mes)
+    med = median(l)
+    print("Median: %d" % med)
+    start = int(roundup(med))
+    while(start < 1000):
+        next = start+100
+        over = list(filter(lambda x: (x>= start and x< next), l))
+        print("Over %d ms: %d" % (start, len(over)))
+        start = next
+    over_1000 = list(filter(lambda x: (x>= 1000), l))
+    print("Over 1000 ms: %d" % len(over_1000))
+    print("#################################")
